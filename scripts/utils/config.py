f6cdfbe72d10d67de11ec5f26226ccd8a8dd06a6
Eric Chandler <echandle@alumni.cmu.edu>
2025-03-17 13:37:36 -0500

    new feature to save some files to scratch dir

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -6,10 +6,12 @@ from os.path import dirname
 
 class Config:
     _COLAB_DATA_DIR = "gdrive/MyDrive/Work/quantify-news/data/"
+    _COLAB_SCRATCH_DIR = "/content/"
     _LOCAL_PROJECT_DIR = dirname(dirname(dirname(Path(__file__).resolve())))
     _LOCAL_DATA_DIR = os.path.join(_LOCAL_PROJECT_DIR, "data/")
     _DATA_CONFIG_PATH = os.path.join(_LOCAL_PROJECT_DIR, "config/data_sources.yaml")
     _PARAM_CONFIG_PATH = os.path.join(_LOCAL_PROJECT_DIR, "config/params.yaml")
+
 
     def __init__(self):    
         with (open(self._DATA_CONFIG_PATH, "r") as f,
@@ -20,21 +22,27 @@ class Config:
         
         if "COLAB" in os.environ:
             self.data_dir = self._COLAB_DATA_DIR
+            self.scratch_dir = self._COLAB_SCRATCH_DIR
         else:
             self.data_dir = self._LOCAL_DATA_DIR
+            # Note: Don't need actual separate scratch directory locally.
+            #       This is just a colab thing because it versions gdrive files.
+            self.scratch_dir = self._LOCAL_PROJECT_DIR
 
     def get_data_path(self, data_key: str, makedirs: bool = True):
-        basepath = self._get_value_reduce(data_key)
-        data_path = os.path.join(self.data_dir, basepath)
-        if makedirs:
-            dir_path = os.path.dirname(data_path)
-            os.makedirs(dir_path, exist_ok=True)
-        return data_path
+        return self._get_path(data_key, self.data_dir, makedirs)
     
-    def get_file_path(self, file_key: str):
-        basepath = self._get_value_reduce(file_key)
-        file_path = os.path.join(self._LOCAL_PROJECT_DIR, basepath)
-        return file_path
+    def get_file_path(self, file_key: str, scratch: bool = False, makedirs: bool = True):
+        dir_path = self.scratch_dir if scratch else self._LOCAL_PROJECT_DIR
+        return self._get_path(file_key, dir_path, makedirs)
+    
+    def _get_path(self, key: str, basepath: str, makedirs: bool = True):
+        childpath = self._get_value_reduce(key)
+        full_path = os.path.join(basepath, childpath)
+        if makedirs:
+            dir_path = os.path.dirname(full_path)
+            os.makedirs(dir_path, exist_ok=True)
+        return full_path
     
     def get_param(self, key: str):
         return self._get_value_reduce(key)
